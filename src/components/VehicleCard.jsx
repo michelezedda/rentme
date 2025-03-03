db61c66ba18fdeb6299e47f4f9dec04334b0490e
Michele <mikzed@hotmail.it>
2025-03-03 18:38:36 +0100

    navbar for mobile completed, added hover scale to vehicle card's pic

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -3,19 +3,19 @@ import { Weight, UsersRound, Luggage } from "lucide-react";
 function VehicleCard({ brand, name, img, kg, suitcases, seats, price }) {
   return (
     <>
-      <div className="flex flex-col relative bg-linear-to-b from-white to-amber-700 border-2 border-black text-black rounded-2xl p-4 gap-4 w-100 h-100 justify-evenly">
-        <div>
-          <h2 className="absolute top-10 text-3xl font-semibold">
-            {brand.toUpperCase()} {name}{" "}
-            <p className="text-xl font-normal">or similar</p>
-          </h2>
+      <div className="flex flex-col relative bg-linear-to-b from-white to-amber-700 border-4 border-black text-black rounded-2xl p-4 gap-4 w-100 h-100 justify-evenly cursor-pointer">
+        <div className="flex absolute top-10 items-end gap-2">
+          <h3 className="text-4xl font-semibold">
+            {brand.toUpperCase()} {name}
+          </h3>
+          <p className="text-2xl pb-[1px]">or similar</p>
         </div>
-        <div className="absolute top-28 flex gap-2">
-          <span className="flex text-lg bg-neutral-400 rounded-[99px] items-center px-2 gap-2">
+        <div className="absolute top-22 flex gap-2">
+          <span className="flex text-lg bg-neutral-400 rounded-[99px] items-center px-3 gap-1">
             <UsersRound size={18} />
             {seats} seats
           </span>
-          <span className="flex text-lg bg-neutral-400 rounded-[99px] items-center px-2 gap-2">
+          <span className="flex text-lg bg-neutral-400 rounded-[99px] items-center px-3 gap-1">
             {suitcases ? (
               <>
                 <Luggage size={18} />
@@ -29,8 +29,14 @@ function VehicleCard({ brand, name, img, kg, suitcases, seats, price }) {
             )}
           </span>
         </div>
-        <img src={img} alt={name} className="absolute bottom-12 left-0" />
-        <p className="absolute bottom-10 text-white text-3xl">$ {price}</p>
+        <img
+          src={img}
+          alt={name}
+          className="absolute bottom-18 left-0 hover:scale-110 duration-500"
+        />
+        <p className="absolute bottom-10 text-white text-3xl">
+          <span className="text-5xl font-semibold">$ {price}</span> /day
+        </p>
       </div>
     </>
   );
