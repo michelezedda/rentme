53e49beb18a46781322af28241947c90d4bf269c
Michele <mikzed@hotmail.it>
2025-03-06 13:07:08 +0100

    Checkout page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router";
 import Home from "./components/Home";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </>
